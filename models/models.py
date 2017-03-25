0eef43b4dccf0d1498ab76b254704bb1d1295867
danztty <danztty@gmail.com>
2017-03-25 16:34:16 -0400

    [MOD] ahora se entrelazo el modulo de employee al crear usuario

--- models/models.py
+++ models/models.py
@@ -6,8 +6,8 @@ from odoo import models, fields, api
 class outflow_seat(models.Model):
     _name = 'cash_box.outflow_seat'
 
-    employee = fields.Char(srting='Employee',help='Enter you user',required=True)
-    amount = fields.Integer(string='Amount',help='Enter amount', required=True)
+    employee = fields.Many2one('hr.employee', srting='Employee',help='Enter you user',required=True)
+    amount = fields.Float(string='Amount',help='Enter amount', required=True)
     description = fields.Text(string='Description',help='Enter a description',required=True)
     date = fields.Datetime(string='Date',help='Enter date',required=True)
 
@@ -15,7 +15,7 @@ class outflow_seat(models.Model):
 class inflow_seat(models.Model):
     _name = 'cash_box.inflow_seat'
 
-    employee = fields.Char(srting='Employee', help='Enter you user', required=True)
-    amount = fields.Integer(string='Amount', help='Enter amount', required=True)
+    employee = fields.Many2one('hr.employee', srting='Employee', help='Enter you user', required=True)
+    amount = fields.Float(string='Amount', help='Enter amount', required=True)
     description = fields.Text(string='Description', help='Enter a description', required=True)
     date = fields.Datetime(string='Date', help='Enter date', required=True)
