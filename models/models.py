f57ed808c6d7274f5df94b2eb683b8bbb2555924
Boanerget <antonyprogramacion96@gmail.com>
2017-03-25 14:49:01 -0400

    [mod] Integracion del atributo string a los campos del modelo.

--- models/models.py
+++ models/models.py
@@ -6,16 +6,16 @@ from odoo import models, fields, api
 class outflow_seat(models.Model):
     _name = 'cash_box.outflow_seat'
 
-    user = fields.Char(help='Enter you user',required=True)
-    amount = fields.Integer(help='Enter amount', required=True)
-    description = fields.Text(help='Enter a description',required=True)
-    date = fields.Datetime(help='Enter dates',required=True)
+    employee = fields.Char(srting='Employee',help='Enter you user',required=True)
+    amount = fields.Integer(string='Amount',help='Enter amount', required=True)
+    description = fields.Text(string='Description',help='Enter a description',required=True)
+    date = fields.Datetime(string='Date',help='Enter date',required=True)
 
 
 class inflow_seat(models.Model):
     _name = 'cash_box.inflow_seat'
 
-    user = fields.Char(help='Enter you user', required=True)
-    amount = fields.Integer(help='Enter amount', required=True)
-    description = fields.Text(help='Enter a description', required=True)
-    date = fields.Datetime(help='Enter dates', required=True)
+    employee = fields.Char(srting='Employee', help='Enter you user', required=True)
+    amount = fields.Integer(string='Amount', help='Enter amount', required=True)
+    description = fields.Text(string='Description', help='Enter a description', required=True)
+    date = fields.Datetime(string='Date', help='Enter date', required=True)
