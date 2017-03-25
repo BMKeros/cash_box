15226785dbaf43e6d16da6a70489f3325b57f3ed
Boanerget <antonyprogramacion96@gmail.com>
2017-03-25 14:23:18 -0400

    [mod] Integracion de los atributos help y require en los campos.

--- models/models.py
+++ models/models.py
@@ -2,21 +2,20 @@
 
 from odoo import models, fields, api
 
+
 class outflow_seat(models.Model):
-	# nombre del modele - nombre de la clase
     _name = 'cash_box.outflow_seat'
 
-    user = fields.Char()
-    amount = fields.Integer()
-    description = fields.Char()
-    date = fields.Datetime()
-    # fecha_registro = fields.Date(auto_now_add=True, auto_now=False)
+    user = fields.Char(help='Enter you user',required=True)
+    amount = fields.Integer(help='Enter amount', required=True)
+    description = fields.Text(help='Enter a description',required=True)
+    date = fields.Datetime(help='Enter dates',required=True)
+
 
 class inflow_seat(models.Model):
-    
     _name = 'cash_box.inflow_seat'
 
-    user= fields.Char()
-    amount= fields.Integer()
-    description= fields.Char()
-    date= fields.Datetime()
+    user = fields.Char(help='Enter you user', required=True)
+    amount = fields.Integer(help='Enter amount', required=True)
+    description = fields.Text(help='Enter a description', required=True)
+    date = fields.Datetime(help='Enter dates', required=True)
