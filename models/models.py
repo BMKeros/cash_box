3f5e082ddd8782d3a32511e2410e2c80efa9021f
danztty <danztty@gmail.com>
2017-03-25 16:49:21 -0400

    [MOD] modifique el modelo, para poner el campo date, default

--- models/models.py
+++ models/models.py
@@ -9,7 +9,7 @@ class outflow_seat(models.Model):
     employee = fields.Many2one('hr.employee', srting='Employee',help='Enter you user',required=True)
     amount = fields.Float(string='Amount',help='Enter amount', required=True)
     description = fields.Text(string='Description',help='Enter a description',required=True)
-    date = fields.Datetime(string='Date',help='Enter date',required=True)
+    date = fields.Datetime( Default=fields.Datetime.now(), string='Date',help='Enter date',required=True)
 
 
 class inflow_seat(models.Model):
@@ -18,4 +18,4 @@ class inflow_seat(models.Model):
     employee = fields.Many2one('hr.employee', srting='Employee', help='Enter you user', required=True)
     amount = fields.Float(string='Amount', help='Enter amount', required=True)
     description = fields.Text(string='Description', help='Enter a description', required=True)
-    date = fields.Datetime(string='Date', help='Enter date', required=True)
+    date = fields.Datetime( Default=fields.Datetime.now(), string='Date', help='Enter date', required=True )
