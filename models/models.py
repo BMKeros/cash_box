618198846660771a919ddf00e49d0e09a729f3d4
Israel Lugo <hostelixisrael@gmail.com>
2017-04-11 00:48:40 -0400

    [MOD] add new field in model currency_id

--- models/models.py
+++ models/models.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
 
@@ -7,15 +6,41 @@ class outflow_seat(models.Model):
     _name = 'cash_box.outflow_seat'
     _rec_name = "employee"
 
-    employee = fields.Many2one('res.users',default=lambda self: self.env.user, string='Employee', required=True, readonly=True, help='');
-    amount = fields.Float(string='Amount',help='Enter amount', required=True)
-    description = fields.Text(string='Description',help='Enter a description',required=True)
+    @api.model
+    def _get_euro(self):
+        return self.env['res.currency.rate'].search([('rate', '=', 1)], limit=1).currency_id
+
+    @api.model
+    def _get_company_currency(self):
+        # currency_id = self.env['res.users'].browse(self._uid).company_id.currency_id
+        currency_id = self.env['res.company']._company_default_get('cash_box').currency_id
+        return currency_id or self._get_euro()
+
+    currency_id = fields.Many2one('res.currency', string='Currency',
+                                  default=lambda self: self._get_company_currency())
+    employee = fields.Many2one('res.users', default=lambda self: self.env.user, string='Employee', required=True,
+                               readonly=True, help='')
+    amount = fields.Monetary(string='Amount', help='Enter amount', required=True, currency_field='currency_id')
+    description = fields.Text(string='Description', help='Enter a description', required=True)
 
 
 class inflow_seat(models.Model):
     _name = 'cash_box.inflow_seat'
     _rec_name = "employee"
 
-    employee = fields.Many2one('res.users',default=lambda self: self.env.user, string='Employee', required=True, readonly=True, help='');
-    amount = fields.Float(string='Amount', help='Enter amount', required=True)
+    @api.model
+    def _get_euro(self):
+        return self.env['res.currency.rate'].search([('rate', '=', 1)], limit=1).currency_id
+
+    @api.model
+    def _get_company_currency(self):
+        # currency_id = self.env['res.users'].browse(self._uid).company_id.currency_id
+        currency_id = self.env['res.company']._company_default_get('cash_box').currency_id
+        return currency_id or self._get_euro()
+
+    currency_id = fields.Many2one('res.currency', string='Currency',
+                                  default=lambda self: self._get_company_currency())
+    employee = fields.Many2one('res.users', default=lambda self: self.env.user, string='Employee', required=True,
+                               readonly=True, help='')
+    amount = fields.Monetary(string='Amount', help='Enter amount', required=True, currency_field='currency_id')
     description = fields.Text(string='Description', help='Enter a description', required=True)
