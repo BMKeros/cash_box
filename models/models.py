9a7be22d73fe86687e8e5a767afb81c42a723b0e
Israel Lugo <hostelixisrael@gmail.com>
2017-04-12 15:23:19 -0400

    [MOD] change logic in default name employee, add function get_related_employee

--- models/models.py
+++ models/models.py
@@ -28,6 +28,11 @@ class inflow_seat(models.Model):
     _name = 'cash_box.inflow_seat'
     _rec_name = "employee"
 
+
+    @api.multi
+    def _get_related_employee(self):
+         return self.env['hr.employee'].search([('user_id', '=', self.env.user.id)])
+
     @api.model
     def _get_euro(self):
         return self.env['res.currency.rate'].search([('rate', '=', 1)], limit=1).currency_id
@@ -40,7 +45,7 @@ class inflow_seat(models.Model):
 
     currency_id = fields.Many2one('res.currency', string='Currency',
                                   default=lambda self: self._get_company_currency())
-    employee = fields.Many2one('res.users', default=lambda self: self.env.user, string='Employee', required=True,
+    employee = fields.Many2one('hr.employee', default=lambda self: self._get_related_employees(), string='Employee', required=True,
                                readonly=True, help='')
     amount = fields.Monetary(string='Amount', help='Enter amount', required=True, currency_field='currency_id')
     description = fields.Text(string='Description', help='Enter a description', required=True)
